37dda8a8a70123408c0340482c0c2799a2dfc0a5
AVER7777 <hogu.verlande@gmail.com>
2025-11-16 23:42:55 +0100

    Created the tables for the database Added the pool to handle connections to postgre with dotenv

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+// PostgreSQL uses a connection pool to manage multiple connections and avoid
+// creating a new connection for each query
+export const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
