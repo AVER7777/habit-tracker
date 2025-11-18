dc7482ec3b7cc38f0c66ca7e7079a5c3a09942f1
AVER7777 <hogu.verlande@gmail.com>
2025-11-18 17:57:25 +0100

    Updated email and password validation regexes

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,9 +1,11 @@
 export function validateEmail(email) {
-    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // Standard email format
+    const regex = /^(?=.{1,254}$)(([^<>()[\]\\.,;:\s@"]{1,64}(\.[^<>()[\]\\.,;:\s@"]{1,64})*)|(".+"))@((\[[0-9]{1,3}(\.[0-9]{1,3}){3}\])|(([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,}))$/;
     return regex.test(String(email).toLowerCase());
 }
 
 export function validatePassword(password) {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/;
+    // Min 8 chars, at least 1 uppercase, 1 lowercase, 1 digit, no 3 identical consecutive chars
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*(.)\1\1).{8,32}$/;
     return regex.test(String(password));
 }
