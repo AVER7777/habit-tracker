eca78871c691e3da7b44e962209aca2654c04569
AVER7777 <hogu.verlande@gmail.com>
2025-11-18 18:07:37 +0100

    Implemented login creating JWT for the user

---
+++ backend/controllers/authController.js
@@ -0,0 +1,32 @@
+import {findByEmail} from "../models/usersModel.js";
+import ApiError from "../utils/ApiError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export async function login(req, res, next) {
+    try {
+        const { email, password } = req.body || {};
+
+        const user = await findByEmail(email);
+
+        if (!user) {
+            throw new ApiError("Invalid credentials", 401);
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password_hash);
+
+        if (!validPassword) {
+            throw new ApiError("Invalid credentials", 401);
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ token, user: { id: user.id, name: user.name } });
+    }
+    catch (error) {
+        next(error);
+    }
+}
