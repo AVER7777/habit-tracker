251f9e18450ee50defd57ccd421f1a43d19b3cc2
AVER7777 <hogu.verlande@gmail.com>
2025-11-18 18:11:33 +0100

    Added functions to update the name, email and password of the user - added error handling

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -1,27 +1,85 @@
 import { pool } from "../db.js";
+import {handleDbError} from "../utils/handleDbError.js";
 
 export async function create({ name, email, password_hash }) {
-    const { rows } = await pool.query(
-        `INSERT INTO users (name, email, password_hash) 
-         VALUES ($1, $2, $3)
-         RETURNING *`,
-        [name, email, password_hash]
-    );
-    return rows[0];
+    try {
+        const { rows } = await pool.query(
+            `INSERT INTO users (name, email, password_hash) 
+             VALUES ($1, $2, $3)
+             RETURNING *`,
+            [name, email, password_hash]
+        );
+        return rows[0];
+    }
+    catch (error) {
+        handleDbError(error);
+    }
 }
 
 export async function findById(id) {
-    const { rows } = await pool.query(
-        'SELECT * FROM users WHERE id = $1',
-        [id]
-    );
-    return rows[0] ?? null;
+    try {
+        const { rows } = await pool.query(
+            'SELECT * FROM users WHERE id = $1',
+            [id]
+        );
+        return rows[0] ?? null;
+    }
+    catch (error) {
+        handleDbError(error);
+    }
 }
 
 export async function findByEmail(email) {
-    const { rows } = await pool.query(
-        `SELECT * FROM users WHERE email = $1`,
-        [email]
-    );
-    return rows[0] ?? null;
+    try {
+        const { rows } = await pool.query(
+            'SELECT * FROM users WHERE email = $1',
+            [email]
+        );
+        return rows[0] ?? null;
+    }
+    catch (error) {
+        handleDbError(error);
+    }
+}
+
+export async function updateEmail(id, email) {
+    try {
+        const { rows } = await pool.query(
+            `UPDATE users SET email = $1 WHERE id = $2
+            RETURNING *`,
+            [email, id]
+        );
+        return rows[0] ?? null;
+    }
+    catch (error) {
+        handleDbError(error);
+    }
+}
+
+export async function updateName(id, name) {
+    try {
+        const { rows } = await pool.query(
+            `UPDATE users SET name = $1 WHERE id = $2
+            RETURNING *`,
+            [name, id]
+        );
+        return rows[0] ?? null;
+    }
+    catch (error) {
+        handleDbError(error);
+    }
+}
+
+export async function updatePassword(id, password_hash) {
+    try {
+        const { rows } = await pool.query(
+            `UPDATE users SET password_hash = $1 WHERE id = $2
+            RETURNING *`,
+            [password_hash, id]
+        );
+        return rows[0] ?? null;
+    }
+    catch (error) {
+        handleDbError(error);
+    }
 }
