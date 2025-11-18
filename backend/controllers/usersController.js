5380c07761f3a5265c3c603d7f48273f1d62fcaf
AVER7777 <hogu.verlande@gmail.com>
2025-11-18 18:21:25 +0100

    Created user update functions for the controller

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,31 +1,31 @@
 import bcrypt from 'bcrypt';
-import {create, findByEmail, findById} from "../models/usersModel.js";
+import {create, findByEmail, findById, updateEmail, updateName, updatePassword} from "../models/usersModel.js";
 import { userDTO } from "../dtos/userDTO.js";
 import {validateEmail, validatePassword} from "../utils/validation.js";
 import ApiError from "../utils/ApiError.js";
 
 export async function createUser(req, res, next) {
     try {
-        const { name, email, password } = req.body;
+        const { name, email, password } = req.body || {};
 
         // Validation
         if (!name || name.length < 3) {
-            throw new ApiError("Name must be at least 3 characters long");
+            throw new ApiError("Name must be at least 3 characters long", 422);
         }
 
         if (!email || !validateEmail(email)) {
-            throw new ApiError("Invalid email address");
+            throw new ApiError("Invalid email address", 422);
         }
 
         if (!password || !validatePassword(password)) {
-            throw new ApiError("Invalid password format");
+            throw new ApiError("Invalid password format", 422);
         }
 
         // Verifying email
-        const existingUser = await findByEmail(email);
+        const userExists = await findByEmail(email);
 
-        if (existingUser) {
-            throw new ApiError("Email already in use")
+        if (userExists) {
+            throw new ApiError("Email already in use", 409)
         }
 
         // Password hashing
@@ -47,6 +47,93 @@ export async function getUserById(req, res, next) {
     try {
         const user = await findById(userId);
         res.json(userDTO(user));
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export async function updateUserEmail(req, res, next) {
+    try {
+        const { email } = req.body || {};
+        const userId = req.user.id;
+
+        if (!validateEmail(email)) {
+            throw new ApiError("Invalid email address", 422);
+        }
+
+        const user = await findById(userId);
+
+        if (!user) {
+            throw new ApiError("User not found", 404);
+        }
+
+        if (email === user.email) {
+            return res.status(200).json(userDTO(user));
+        }
+
+        const updatedUser = await updateEmail(userId, email);
+
+        res.status(200).json(userDTO(updatedUser));
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export async function updateUserName(req, res, next) {
+    try {
+        const { name } = req.body || {};
+        const userId = req.user.id;
+
+        if (!name || name.length < 3) {
+            throw new ApiError("Name must be at least 3 characters long", 422);
+        }
+
+        const user = await findById(userId);
+
+        if (!user) {
+            throw new ApiError("User not found", 404);
+        }
+
+        if (name === user.name) {
+            return res.status(200).json(userDTO(user));
+        }
+
+        const updatedUser = await updateName(userId, name);
+
+        res.status(200).json(userDTO(updatedUser));
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export async function updateUserPassword(req, res, next) {
+    try {
+        const { password } = req.body || {};
+        const userId = req.user.id;
+
+        if (!validatePassword(password)) {
+            throw new ApiError("Invalid password format", 422);
+        }
+
+        const user = await findById(userId);
+
+        if (!user) {
+            throw new ApiError("User not found", 404);
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            return res.status(200).json(userDTO(user));
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const updatedUser = await updatePassword(userId, hashedPassword);
+
+        res.status(200).json(userDTO(updatedUser));
     }
     catch (error) {
         next(error);
