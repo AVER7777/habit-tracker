707054b49d0731740139c9e4a461b15782b83fdf
AVER7777 <hogu.verlande@gmail.com>
2025-11-20 00:35:57 +0100

    Added refresh route for authentication

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { login } from '../controllers/authController.js';
+import { login, refresh } from '../controllers/authController.js';
 
 const router = express.Router();
 
 // Create
 router.post('/login', login);
+router.post('/refresh', refresh);
 
 export default router;
