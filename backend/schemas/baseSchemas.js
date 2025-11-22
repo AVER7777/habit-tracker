ac8d9c6120a8281162c3ebd9c6b1e8099a679ef4
AVER7777 <hogu.verlande@gmail.com>
2025-11-22 17:58:18 +0100

    Added schemas for habits input validation + generic tools

--- backend/schemas/baseSchemas.js
+++ backend/schemas/baseSchemas.js
@@ -1,14 +1,28 @@
 // Schemas
 import { z } from 'zod';
 
-export const emailSchema = z
+export const genericIdSchema = z
+    .union([z.string(), z.number()])
+    .pipe(
+        z.coerce
+            .number()
+            .int('Invalid ID format. Please provide a valid integer.')
+            .positive('ID must be a positive number.'),
+    );
+
+export const genericNumberSchema = z
+    .number({ required_error: 'Number is required' })
+    .int('Invalid number format. Please provide a valid integer.')
+    .positive('Number must be a positive number.');
+
+export const genericEmailSchema = z
     .string({ required_error: 'Email is required' })
     .email({ message: 'Invalid email address' });
 
-// min 8 chars, at least 1 uppercase, 1 lowercase, 1 digit, no 3 identical consecutive chars
+// min 8 chars, at least 1 uppercase, 1 lowercase, 1 digit, not 3 identical consecutive chars
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*(.)\1\1).{8,32}$/;
 
-export const passwordSchema = z
+export const genericPasswordSchema = z
     .string({
         required_error: 'Password is required',
     })
@@ -17,6 +31,6 @@ export const passwordSchema = z
             'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit.',
     });
 
-export const nameSchema = z
+export const genericNameSchema = z
     .string({ required_error: 'Name is required' })
     .min(3, { message: 'Name must be at least 3 characters long' });
---
+++ backend/schemas/habitSchema.js
@@ -0,0 +1,88 @@
+import { z } from 'zod';
+
+import { genericIdSchema, genericNameSchema, genericNumberSchema } from './baseSchemas.js';
+
+// Schemas
+const colorRegex = /^#[0-9A-F]{6}$/;
+
+const colorSchema = z
+    .string({
+        required_error: 'Color is required',
+    })
+    .regex(colorRegex, {
+        message: 'Invalid color format. Please use a hexadecimal color code (e.g., #FF0000).',
+    });
+
+const frequencySchema = z
+    .number({
+        required_error: 'Frequency is required',
+    })
+    .int({ message: 'Frequency must be an integer' })
+    .min(1, { message: 'Frequency must be at least 1' })
+    .max(7, { message: 'Frequency must be at most 7' });
+
+// Export
+export const createHabitSchema = z.object({
+    body: z.object({
+        name: genericNameSchema,
+        color: colorSchema,
+        frequency: frequencySchema,
+    }),
+});
+
+export const getAllHabitsSchema = z.object({
+    params: z.object({
+        id: genericIdSchema,
+    }),
+});
+
+export const updateHabitNameSchema = z.object({
+    params: z.object({
+        id: genericIdSchema,
+    }),
+    body: z.object({
+        name: genericNameSchema,
+    }),
+});
+
+export const updateHabitColorSchema = z.object({
+    params: z.object({
+        id: genericIdSchema,
+    }),
+    body: z.object({
+        color: colorSchema,
+    }),
+});
+
+export const updateHabitFrequencySchema = z.object({
+    params: z.object({
+        id: genericIdSchema,
+    }),
+    body: z.object({
+        frequency: frequencySchema,
+    }),
+});
+
+export const updateHabitCurrentStreakSchema = z.object({
+    params: z.object({
+        id: genericIdSchema,
+    }),
+    body: z.object({
+        currentStreak: genericNumberSchema,
+    }),
+});
+
+export const incrementHabitCurrentStreakSchema = z.object({
+    params: z.object({
+        id: genericIdSchema,
+    }),
+});
+
+export const updateHabitMaxStreakSchema = z.object({
+    params: z.object({
+        id: genericIdSchema,
+    }),
+    body: z.object({
+        maxStreak: genericNumberSchema,
+    }),
+});
