cae6f43aa40f537ab986b647f0d4576e77680f41
AVER7777 <hogu.verlande@gmail.com>
2025-12-13 10:57:59 +0100

    feat(front:hooks): create hook that handle long press

---
+++ frontend/src/hooks/useLongPress.jsx
@@ -0,0 +1,68 @@
+import { useCallback, useRef, useState } from 'react';
+
+const useLongPress = (
+    onLongPress,
+    onClick = () => {},
+    { shouldPreventDefault = true, delay = 500 } = {},
+) => {
+    const [longPressTriggered, setLongPressTriggered] = useState(false);
+    const timeout = useRef();
+    const target = useRef();
+
+    const start = useCallback(
+        (event) => {
+            if (shouldPreventDefault && event.target) {
+                event.target.addEventListener('touchend', preventDefault, {
+                    passive: false,
+                });
+                target.current = event.target;
+            }
+
+            timeout.current = setTimeout(() => {
+                onLongPress(event);
+                setLongPressTriggered(true);
+            }, delay);
+        },
+        [onLongPress, delay, shouldPreventDefault],
+    );
+
+    const clear = useCallback(
+        (event, shouldTriggerClick = true) => {
+            timeout.current && clearTimeout(timeout.current);
+
+            if (shouldTriggerClick && !longPressTriggered) {
+                onClick(event);
+            }
+
+            setLongPressTriggered(false);
+
+            if (shouldPreventDefault && target.current) {
+                target.current.removeEventListener('touchend', preventDefault);
+            }
+        },
+        [shouldPreventDefault, onClick, longPressTriggered],
+    );
+
+    return {
+        onMouseDown: (e) => start(e),
+        onTouchStart: (e) => start(e),
+        onMouseUp: (e) => clear(e),
+        onTouchEnd: (e) => clear(e),
+    };
+};
+
+const preventDefault = (e) => {
+    if (!isTouchEvent(e)) {
+        return;
+    }
+
+    if (e.touches.length < 2 && e.preventDefault) {
+        e.preventDefault();
+    }
+};
+
+const isTouchEvent = (e) => {
+    return e && 'touches' in e;
+};
+
+export default useLongPress;
