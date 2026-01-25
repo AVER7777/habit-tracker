b5ccf8b32accc63c37142e48f607fd1a1bef4de5
AVER7777 <hogu.verlande@gmail.com>
2026-01-25 10:59:26 +0100

    feat(front:style): big update for habit card (emoji not implemented yet)

--- frontend/src/components/habits/HabitCard.jsx
+++ frontend/src/components/habits/HabitCard.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
-
 import { useNavigate } from 'react-router-dom';
+
 import { addEntry, deleteEntry } from '../../api/entries.js';
 import { getHabit } from '../../api/habits.js';
 import useLongPress from '../../hooks/useLongPress.jsx';
 import { getCurrentWeekDates, getLocalDate } from '../../utils/date.js';
+import FlameIcon from '../icons/FlameIcon';
+import ProgressCircleIcon from '../icons/ProgressCircleIcon.jsx';
 
 function HabitCard({ habit }) {
     const navigate = useNavigate();
@@ -19,7 +21,18 @@ function HabitCard({ habit }) {
         navigate(`/habits/${habit.id}/settings`);
     };
 
+    // animation
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    const triggerFeedback = () => {
+        if (navigator.vibrate) navigator.vibrate(15);
+
+        setIsAnimating(true);
+        setTimeout(() => setIsAnimating(false), 200);
+    };
+
     const handleClick = async () => {
+        triggerFeedback();
         try {
             setChecked(!checked);
 
@@ -58,48 +71,98 @@ function HabitCard({ habit }) {
 
     const weekDays = getCurrentWeekDates(1);
 
+    let label;
+
+    if (habit.frequency === 7) {
+        if (habit.currentStreak === 1) {
+            label = 'DAY';
+        } else {
+            label = 'DAYS';
+        }
+    } else {
+        if (habit.currentStreak <= 14) {
+            label = 'WEEK';
+        } else {
+            label = 'WEEKS';
+        }
+    }
+
     return (
         <div
-            className={`mb-4 p-4 rounded-3xl relative overflow-hidden transition-all flex flex-col justify-between min-h-[150px] cursor-pointer select-none`}
+            className={`border p-4 border-ios-quaternary aspect-square rounded-3xl grid grid-rows-[auto_1fr_auto]
+                ${!checked && 'bg-ios-bg'}
+                ${isAnimating ? 'animate-bump' : ''}
+            `}
             style={{
-                backgroundColor: habit.color,
-                color: 'white',
+                color: checked ? 'white' : undefined,
+                background: checked
+                    ? `linear-gradient(to bottom, var(--ios-${habit.color}-dark), var(--habit-darker))`
+                    : undefined,
+                '--habit-darker': `color-mix(in srgb, var(--ios-${habit.color}-dark), black 60%)`,
             }}
             {...longPressEvent}
         >
-            <div className="flex justify-between items-start">
-                <div className="flex flex-col">
-                    <div className="flex items-center gap-1 opacity-90 mb-1">
-                        <span className="text-xl">🔥</span>
-                        <span className="font-bold text-lg">{currentStreak}</span>
-                        <span className="text-xs uppercase opacity-75 pt-1">Jours</span>
+            <div className="flex flex-row justify-between">
+                <div className="ml-1">
+                    <div className="flex flex-col items-start justify-between w-full">
+                        <div className="flex items-center">
+                            {/*streak*/}
+                            <span className="pr-1 text-[32px] font-bold leading-none">
+                                {currentStreak}
+                            </span>
+
+                            {/*flame*/}
+                            <FlameIcon
+                                className="w-auto h-5 translate-y-1"
+                                style={{
+                                    color: `var(--ios-${habit.color})`,
+                                }}
+                            />
+                        </div>
+
+                        <span className="text-[11px] font-medium justify-left text-ios-secondary">
+                            {label}
+                        </span>
                     </div>
-                    <h3 className="text-2xl font-bold leading-tight">{habit.name}</h3>
                 </div>
 
-                <div
-                    className={`w-8 h-8 rounded-full flex items-center justify-center border-2 border-white/30 transition-colors ${checked ? 'bg-white text-black' : 'bg-transparent'}`}
-                >
-                    {checked && <span className="font-bold">✓</span>}
+                {/* progress circle */}
+                <div className="flex items-center justify-center">
+                    <ProgressCircleIcon
+                        current={completedDates.length}
+                        total={habit.frequency}
+                        size={46}
+                        color={`var(--ios-${habit.color}-light)`}
+                        checked={checked}
+                    />
                 </div>
             </div>
 
-            <div className="flex justify-between items-center mt-4 pt-4 border-t border-white/20">
+            {/*habit name*/}
+            <div className="flex items-center w-full overflow-hidden">
+                <span className="text-lg shrink-0 mr-1">😊</span>
+                <h3 className="text-[20px] tracking-tight leading-4 font-bold line-clamp-2 text-left break-words">
+                    {habit.name}
+                </h3>
+            </div>
+
+            {/*week days*/}
+            <div className="flex justify-between items-center border-white mb-1">
                 {weekDays.map((day, index) => {
                     const isDayCompleted = completedDates.includes(day.dateString);
 
                     return (
                         <div key={index} className="flex flex-col items-center gap-1">
                             <div
-                                className={`
-                                    w-6 h-6 rounded-md flex items-center justify-center text-[10px] font-bold transition-all
-                                    ${
-                                        isDayCompleted
-                                            ? 'bg-white text-black opacity-100'
-                                            : 'bg-black/20 text-white opacity-60'
-                                    }
-                                    ${day.isToday ? 'ring-2 ring-white ring-offset-1 ring-offset-transparent' : ''}
-                                `}
+                                className="w-4 h-4 rounded-[3px] flex items-center justify-center text-[9px] font-bold transition-all"
+                                style={{
+                                    color: isDayCompleted
+                                        ? 'white'
+                                        : `var(--ios-${habit.color}-light)`,
+                                    backgroundColor: isDayCompleted
+                                        ? `var(--ios-${habit.color}-light)`
+                                        : `var(--ios-${habit.color}-dark)`,
+                                }}
                             >
                                 {day.letter}
                             </div>
